c827cf5f8ccbbb40dba3417b5421f06bcee76730
pommedeterresautee <pommedeterresautee@msn.com>
2019-03-11 10:44:59 +0100

    tests cover

---
+++ tests/testthat/test-stemmer.R
@@ -0,0 +1,30 @@
+context("test all stemmers")
+
+test_that("French - test simple words", {
+  expect_equal(object = french_stemmer(words = c("tester", "testament", "chevaux", "aromatique", "personnel", "folle", "acheteuse")),
+               expected = c("test", "testament", "cheval", "aromat", "personel", "fou", "acheter"))
+})
+
+test_that("French - test words with accents", {
+  expect_equal(object = french_stemmer(words = c("complète", "caisière")),
+               expected = c("complet", "caisier"))
+})
+
+test_that("German - test words with accents", {
+  expect_equal(object = french_stemmer(words = c("kinder")),
+               expected = c("kind"))
+})
+
+test_that("Spanish - test words with accents", {
+  expect_equal(object = french_stemmer(words = c("perros")),
+               expected = c("pero"))
+})
+
+test_that("test empty word", {
+  expect_equal(object = french_stemmer(words = c("")),
+               expected = c(""))
+  expect_equal(object = german_stemmer(words = c("")),
+               expected = c(""))
+  expect_equal(object = spanish_stemmer(words = c("")),
+               expected = c(""))
+})
